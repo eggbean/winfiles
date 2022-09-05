9e42cf8bd1e0cde5db1992ad7b16c2aafc7adfb4
eggbean <815311+eggbean@users.noreply.github.com>
2022-09-05 17:33:22 +0100

    Add more doskey aliases

--- bin/clink/aliases.cmd
+++ bin/clink/aliases.cmd
@@ -1,4 +1,6 @@
 @echo off
-doskey ll=ls -l
 doskey hosts=gvim.exe C:\Windows\System32\drivers\etc\hosts
 doskey aliases=gvim.exe %USERPROFILE%\bin\clink\aliases.cmd
+doskey ll=ls -l
+doskey cd=cdd.cmd
+doskey sudo=gsudo
