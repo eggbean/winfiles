213824d271fefdb39ce8469c94e4d99a7cc8c897
eggbean <815311+eggbean@users.noreply.github.com>
2022-10-16 20:03:25 +0100

    Update scripts

--- scripts/fix_icons.cmd
+++ scripts/fix_icons.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+:: Sets icons for various folders
+
 :: Fix icons for Creative Cloud sync
 pushd "C:\Program Files (x86)\Adobe\Adobe Sync\CoreSync\sibres\CloudSync"
 copy "%USERPROFILE%\winfiles\icons\my_icons\cloud_fld_w10.ico" .
@@ -21,5 +23,88 @@ pushd %USERPROFILE%\vimfiles
 del /a ash desktop.ini
 echo [.ShellClassInfo] > desktop.ini
 echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\vimfiles.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set google drive folder icon
+pushd %USERPROFILE%\"Google Drive"
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo InfoTip=Your Google Drive folder contains files that you're syncing with Google. >> desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\google_drive.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set icloud drive folder icon
+pushd %USERPROFILE%\iCloudDrive
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo LocalizedResourceName=iCloud Drive >> desktop.ini
+echo InfoTip=iCloud Drive >> desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\iCloud Folder.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set bin folder icon
+pushd %USERPROFILE%\winfiles\bin
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\bat.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set icons folder icon
+pushd %USERPROFILE%\winfiles\icons
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Apps Folder.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set my_icons folder icon
+pushd %USERPROFILE%\winfiles\icons\my_icons
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Apps Folder.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set installers folder icon
+pushd %USERPROFILE%\winfiles\Installers
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Software Folder Icon.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set reg files folder icon
+pushd %USERPROFILE%\winfiles\reg
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Registry Folder Icon.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set scripts folder icon
+pushd %USERPROFILE%\winfiles\scripts
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\VBS Folder.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set settings folder icon
+pushd %USERPROFILE%\winfiles\Settings
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Batch Folder Icon.ico,0 >> desktop.ini
+attrib +a +s +h desktop.ini
+popd
+
+:: Set windows terminal settings folder icon
+pushd %USERPROFILE%\winfiles\Windows_Terminal
+del /a ash desktop.ini
+echo [.ShellClassInfo] > desktop.ini
+echo IconResource=%USERPROFILE%\winfiles\icons\my_icons\Batch Folder Icon.ico,0 >> desktop.ini
 attrib +a +s +h desktop.ini
 popd
