c08f90c74b08c372e59c59165d7af0e9fb2bf228
eggbean <815311+eggbean@users.noreply.github.com>
2023-03-23 05:48:38 +0000

    Replace utils-coreutils ls with exa.exe

--- scripts/clink_start.cmd
+++ scripts/clink_start.cmd
@@ -17,9 +17,9 @@ doskey startmenu=pushd %APPDATA%\Microsoft\Windows\Start Menu\Programs
 doskey home=pushd %USERPROFILE%
 doskey sudo=gsudo $*
 doskey cd=cdd.cmd $*
-doskey ls=ls -h -N --color=auto $*
-doskey ll=ls -h -l -N --color=auto $*
-doskey lla=ls -h -Al -N --color=auto $*
+doskey ls=exa $*
+doskey ll=exa -l --git $*
+doskey lla=exa -al --git $*
 doskey cp=cp -i $*
 doskey mv=mv -i $*
 doskey rm=rm -i $*
