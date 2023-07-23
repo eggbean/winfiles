e0b7bb9657bd0b0dd0cedd5378be523180c15f4c
eggbean <815311+eggbean@users.noreply.github.com>
2023-07-23 15:12:07 +0100

    Add script to install essential Windows packages

---
+++ scripts/install_packages.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+winget install -e --id chrisant996.Clink
+winget install -e --id hluk.CopyQ
+winget install -e --id Git.Git
+winget install -e --id GitHub.cli
+winget install -e --id Google.Chrome
+winget install -e --id gerardog.gsudo
+winget install -e --id Logitech.OptionsPlus
+winget install -e --id Microsoft.VCRedist.2015+.x64
+winget install -e --id Microsoft.VCRedist.2015+.x86
+winget install -e --id Mozilla.Firefox
+winget install -e --id M2Team.NanaZip
+winget install -e --id Notion.Notion
+winget install -e --id Microsoft.PowerToys
+winget install -e --id qutebrowser.qutebrowser -l "C:\Program Files\qutebrowser"
+winget install -e --id vim.vim
+winget install -e --id WinDirStat.WinDirStat
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id WinSCP.WinSCP
