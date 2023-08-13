24b2739ac9fda52ef6b0ce700f3335f2bc5ee1aa
eggbean <815311+eggbean@users.noreply.github.com>
2023-08-13 11:49:42 +0100

    Add %OLDPWD% and ~ for %USERPROFILE% to cdd.cmd!

--- bin/cdd.cmd
+++ bin/cdd.cmd
@@ -1,6 +1,12 @@
 @echo off
+set dest=%*
+if '%dest%'=='' goto :cdd
+if '%dest:~0,1%' == '~' set dest=%USERPROFILE%%dest:~1%
+set OLDPWD="%cd%"
+:cdd
 set pushd_tmp=%TEMP%\pushd.tmp
 set cdd_tmp_cmd=%TEMP%\cdd.tmp.cmd
 pushd > %pushd_tmp%
-%~dps0_cdd.exe %* < %pushd_tmp% > %cdd_tmp_cmd%
+%~dps0_cdd.exe %dest% < %pushd_tmp% > %cdd_tmp_cmd%
+set dest=
 %cdd_tmp_cmd%
