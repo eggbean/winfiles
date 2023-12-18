c74d32034a7b85cb808d4005ba7438f071917e68
Jason Gomez <815311+eggbean@users.noreply.github.com>
2023-12-18 23:41:13 +0000

    Improve eza-wrapper.cmd script
    Still need to add reversal of sorting.

--- scripts/eza-wrapper.cmd
+++ scripts/eza-wrapper.cmd
@@ -12,14 +12,21 @@ set "ico=0"
 set "hed=0"
 set "gpd=0"
 set "col=1"
+set "snu=0"
+set "sac=0"
+set "scr=0"
+set "ssz=0"
 
 :: Parse options
 :parse_options
 if "%1"=="" goto :end_parse_options
-if /i "%1"=="-a" set "dot=1" & set "hru=0"
-if /i "%1"=="-l" set "hed=1"
-if /i "%1"=="-h" set "hru=1" & set "meb=0"
+if /i "%1"=="-a" set "dot=1"
+if /i "%1"=="-h" set "hru=1"
 if /i "%1"=="-g" set "git=0"
+if /i "%1"=="-t" set "snu=1"
+if /i "%1"=="-u" set "sac=1"
+if /i "%1"=="-c" set "scr=1"
+if /i "%1"=="-S" set "ssz=1"
 if /i "%1"=="--icons" set "ico=1"
 if /i "%1"=="--color=never" set "col=0"
 shift
@@ -30,13 +37,17 @@ goto :parse_options
 :: Build eza command with options
 set "eza_opts="
 if %dot% equ 1 set "eza_opts=-a"
-if %hru% equ 1 set "eza_opts=!eza_opts! -h"
+if %snu% equ 1 set "eza_opts=!eza_opts! -s modified"
+if %sac% equ 1 set "eza_opts=!eza_opts! -us accessed"
+if %scr% equ 1 set "eza_opts=!eza_opts! -Us created"
+if %ssz% equ 1 set "eza_opts=!eza_opts! -s size"
+if %hru% equ 1 set "eza_opts=!eza_opts! -B"
 if %meb% equ 1 set "eza_opts=!eza_opts! -b"
 if %fgp% equ 1 set "eza_opts=!eza_opts! -g"
 if %lnk% equ 1 set "eza_opts=!eza_opts! -H"
 if %git% equ 1 (
   git rev-parse --is-inside-work-tree >nul 2>nul
-  if !errorlevel! equ 0 set "eza_opts=!eza_opts! --git"
+  if !errorlevel! equ 0 set "eza_opts=%eza_opts% --git"
 )
 if %ico% equ 1 set "eza_opts=!eza_opts! --icons"
 if %hed% equ 1 set "eza_opts=!eza_opts! -h"
