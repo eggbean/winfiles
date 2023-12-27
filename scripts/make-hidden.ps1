14200f2e9bc3b4f4efb89de54e45ef2ffba456a3
eggbean <815311+eggbean@users.noreply.github.com>
2023-12-27 22:23:54 +0000

    Update .hidden file creation script
    I've been using this for a while but not committed it until I fixed a couple of
    minor problems which are still not fixed:
    
    1. There is no trailing end-of-line character on the last line
    2. The .hidden files do not have the hidden attribute set themselves
    3. The script seems to be applying the hidden attribute to normal document files

--- scripts/make-hidden.ps1
+++ scripts/make-hidden.ps1
@@ -1,16 +1,38 @@
-$param1=$args[0]
-Get-ChildItem -LiteralPath $param1 -Force -Directory -Recurse | ForEach-Object {
-    if ($hiddenItems = $_ | Get-ChildItem -Hidden -Force -Name) {
-        # Creates BOM-less UTF-8 files with CRLF line endings and a trailing newline
-        New-Item -Force (Join-Path $_.FullName '.hidden') -Value (($hiddenItems -join "`n") + "`n")
-    } else {
-        $hiddenFile = Join-Path $_.FullName '.hidden'
-        if (Test-Path $hiddenFile) {
-            Remove-Item $hiddenFile
-        }
-    }
-    Get-ChildItem -Path $_.FullName -Recurse -Filter ".*" | ForEach-Object {
-        $_.Attributes = $_.Attributes -bor [System.IO.FileAttributes]::Hidden
-    }
+$param1 = $args[0]
+
+# Create .hidden files in the top-level directory
+$hiddenItemsTopLevel = Get-ChildItem -LiteralPath $param1 -Force -Directory -Attributes !ReparsePoint | Where-Object { $_.Name -notmatch '^\.' }
+
+foreach ($item in $hiddenItemsTopLevel) {
+    $hiddenFilePath = Join-Path $item.FullName '.hidden'
+    $null = New-Item -Force -Path $hiddenFilePath -Value $null
 }
 
+# Apply hidden attribute to files starting with a dot in the top-level directory
+Get-ChildItem -Path $param1 -File | Where-Object { $_.Name -match '^\.' } | ForEach-Object {
+    $_.Attributes = $_.Attributes -bor [System.IO.FileAttributes]::Hidden
+}
+
+if ($param1 -ne $HOME) {
+    # Traverse subdirectories and create .hidden files as needed
+    Get-ChildItem -LiteralPath $param1 -Force -Directory -Recurse -Attributes !ReparsePoint | ForEach-Object {
+        $dir = $_
+        $hiddenItems = Get-ChildItem -Path $dir.FullName -Force | Where-Object { ($_.Attributes -band [System.IO.FileAttributes]::Hidden) -and ($_.Attributes -band [System.IO.FileAttributes]::Directory) -eq 0 }
+
+        if ($hiddenItems.Count -gt 0) {
+            $hiddenContent = $hiddenItems.Name -join "`n"
+            $hiddenFilePath = Join-Path $dir.FullName '.hidden'
+            $hiddenContent | Set-Content -Path $hiddenFilePath -Force -NoNewline
+        } else {
+            $hiddenFilePath = Join-Path $dir.FullName '.hidden'
+            Remove-Item $hiddenFilePath -Force -ErrorAction SilentlyContinue
+        }
+
+        # Apply hidden attribute to files in subdirectories
+        Get-ChildItem -Path $_.FullName -File | ForEach-Object {
+            if ($_.Name -notmatch '^\.') {
+                $_.Attributes = $_.Attributes -bor [System.IO.FileAttributes]::Hidden
+            }
+        }
+    }
+}
