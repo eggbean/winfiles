1584df271398ccdfffaf8db1d0e5904a8cc66575
eggbean <815311+eggbean@users.noreply.github.com>
2023-12-30 19:06:56 +0000

    Add script to pull git repositories from remote

---
+++ scripts/pull-repos.cmd
@@ -0,0 +1,29 @@
+@echo off
+setlocal
+
+call :processRepo "%USERPROFILE%\winfiles"
+call :processRepo "%USERPROFILE%\.dotfiles"
+
+goto :eof
+
+:processRepo
+set "REPO_DIR=%~1"
+set STASHED=0
+
+git -C "%REPO_DIR%" status --porcelain | findstr "^" >nul
+if %ERRORLEVEL% == 0 (
+    echo Changes detected in %REPO_DIR%. Stashing...
+    git -C "%REPO_DIR%" stash
+    set STASHED=1
+)
+
+echo Pulling repository %REPO_DIR%...
+git -C "%REPO_DIR%" pull
+
+if %STASHED% == 1 (
+    echo Popping stash in %REPO_DIR%...
+    git -C "%REPO_DIR%" stash pop
+)
+
+echo Done with %REPO_DIR%.
+exit /b
