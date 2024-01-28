9c7695aa47ebc4991c9047753899ff41db64fe79
eggbean <815311+eggbean@users.noreply.github.com>
2024-01-28 01:04:26 +0000

    Add bypass paywall Chrome extension repos to pull-repos

--- scripts/pull-repos.cmd
+++ scripts/pull-repos.cmd
@@ -1,31 +1,36 @@
 @echo off
 setlocal
 
+call :processRepo "%USERPROFILE%\Documents\Chrome Extensions\bypass-paywalls-chrome-clean"
+call :processRepo "%USERPROFILE%\Documents\Chrome Extensions\bypass-paywalls-chrome-master"
 call :processRepo "%USERPROFILE%\winfiles\Settings\clink-completions"
 call :processRepo "%USERPROFILE%\winfiles\Settings\clink-gizmos"
 call :processRepo "%USERPROFILE%\winfiles"
 call :processRepo "%USERPROFILE%\.dotfiles"
 
-goto :eof
+goto :EOF
 
 :processRepo
 set "REPO_DIR=%~1"
 set STASHED=0
 
-git -C "%REPO_DIR%" status --porcelain | findstr "^" >nul
-if %ERRORLEVEL% == 0 (
-    echo Changes detected in %REPO_DIR%. Stashing...
-    git -C "%REPO_DIR%" stash
-    set STASHED=1
+if exist "%REPO_DIR%" (
+    git -C "%REPO_DIR%" status --porcelain | findstr "^" >nul
+    if %ERRORLEVEL% == 0 (
+        echo Changes detected in %REPO_DIR%. Stashing...
+        git -C "%REPO_DIR%" stash
+        set STASHED=1
+    )
+
+    echo Pulling repository %REPO_DIR%...
+    git -C "%REPO_DIR%" pull --no-ff
+
+    if %STASHED% == 1 (
+        echo Popping stash in %REPO_DIR%...
+        git -C "%REPO_DIR%" stash pop
+    )
+
+    echo Done with %REPO_DIR%.
 )
 
-echo Pulling repository %REPO_DIR%...
-git -C "%REPO_DIR%" pull --no-ff
-
-if %STASHED% == 1 (
-    echo Popping stash in %REPO_DIR%...
-    git -C "%REPO_DIR%" stash pop
-)
-
-echo Done with %REPO_DIR%.
 exit /b
