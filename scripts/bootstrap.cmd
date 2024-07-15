cbfd4ce22840d2073bd60677285602b2d3801912
eggbean <815311+eggbean@users.noreply.github.com>
2024-07-15 21:49:23 +0100

    Improve bootstrap script using functions

--- scripts/bootstrap.cmd
+++ scripts/bootstrap.cmd
@@ -1,27 +1,30 @@
-@echo off
+@echo on
 
-:: Automates the setup and configuration of the Windows environment
+:: Automates the setup and configuration of my Windows environment
 :: * Exclude known false positives from Windows Defender scanning
 :: * Install essential packages using winget
-:: * Install scoop for multi-users and packages through PowerShell script
-:: * Setup OpenSSH and retrieve key from Dashlane
-:: * Retrieves current SSH key from Dashlane vault
-:: * Sets up Clink, cloning relevant repositores for extra features
-:: * Enables Developer Mode (allows symlink creation without elevation)
-:: * Sparse clones Linux dotfiles repository
-:: * Makes symlinks from configurations in this repository and .dotfiles
-:: * Hides dotfiles and dotdirectories in %USERPROFILE% and winfiles
-:: * Makes startup shortcuts for some systray applications
-:: * Installs and registers fonts in font directory
-:: * Installs the wedge redirector for the Chrometana Pro Chrome extension
-:: * Enables hibernation
-:: * Sets other OS settings through registry
+:: * Set icons for various folders
+:: * Add vim to %PATH%
+:: * Install scoop for multi-users and packages (if not already installed)
+:: * Setup OpenSSH and retrieve SSH key from Dashlane vault
+:: * Install the wedge redirector for the Chrometana Pro Chrome extension
+:: * Setup Clink, cloning relevant repositories for extra features
+:: * Sparse checkout Linux dotfiles repository
+:: * Create symlinks between %APPDATA% and Linux dotfiles
+:: * Create symlink for vimfiles from Linux dotfiles repository
+:: * Create symlinks for configs in this repo to %HOME% and %LOCALAPPDATA%
+:: * Make startup shortcuts for some systray applications
+:: * Install and register fonts
+:: * Set various OS settings through the registry
+:: * Enable and Disable Windows Features
+:: * Hide dotfiles and dotdirectories in %USERPROFILE% and winfiles
 
 :: TO DO:
-:: * Translate to pure PowerShell at some point
+:: * Translate to do everything in pure PowerShell (Work In Progress)
 
+:: Check for admin privileges
 net session >nul 2>&1
-if not %ERRORLEVEL% == 0 (
+if %ERRORLEVEL% NEQ 0 (
     echo Not admin/elevated
     exit /b 1
 )
@@ -38,233 +41,137 @@ icacls "%USERPROFILE%\winfiles" /setowner "%USERNAME%" /t
 :: Install essential packages using winget
 call "%~dp0install_packages.cmd"
 
+:: Set icons for various folders
+call "%~dp0fix_icons.cmd"
+
 :: Add vim to %PATH%
 powershell -File "%~dp0add_vim_to_path.ps1"
 
-:: Install scoop for multi-users and packages
-dir %SCOOP% >nul 2>&1
-if not %ERRORLEVEL% == 0 (
+:: Install scoop for multi-users and packages (if not already installed)
+if not exist "%SCOOP%" (
     powershell -File "%~dp0install_scoop.ps1"
 )
 
-:: Setup OpenSSH and retrieve key from Dashlane
-powershell -ExecutionPolicy Bypass -File "%~dp0setup_openssh.ps1"
+:: Setup OpenSSH and retrieve SSH key from Dashlane vault
+powershell -File "%~dp0setup_openssh.ps1"
 
-:: Enable Developer Mode
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Appx" /v "AllowDevelopmentWithoutDevLicense" /t REG_DWORD /d 1 /f >nul
+:: Install the wedge redirector for the Chrometana Pro Chrome extension
+powershell -File "%~dp0install_wedge.ps1"
 
-:: Sparse checkout dotfiles
-if not exist %USERPROFILE%\.dotfiles (
-    cd %USERPROFILE%
+:: Setup Clink, cloning relevant repositores for extra features
+if not exist "%LOCALAPPDATA%\clink" (
+    mkdir "%LOCALAPPDATA%\clink"
+)
+call :CreateSymlink "%LOCALAPPDATA%\clink\clink_start.cmd" "%USERPROFILE%\winfiles\scripts\clink_start.cmd"
+call :CreateSymlink "%LOCALAPPDATA%\clink\clink_settings" "%USERPROFILE%\winfiles\Settings\clink_settings"
+call :CreateSymlink "%LOCALAPPDATA%\clink\_inputrc" "%USERPROFILE%\winfiles\Settings\_inputrc"
+
+if defined CLINK_COMPLETIONS_DIR if not exist "%CLINK_COMPLETIONS_DIR%" (
+    git clone https://github.com/vladimir-kotikov/clink-completions.git "%USERPROFILE%\winfiles\Settings\clink-completions"
+    clink installscripts "%USERPROFILE%\winfiles\Settings\clink-completions"
+)
+
+if not exist "%USERPROFILE%\winfiles\Settings\clink-gizmos" (
+    git clone https://github.com/chrisant996/clink-gizmos.git "%USERPROFILE%\winfiles\Settings\clink-gizmos"
+    clink installscripts "%USERPROFILE%\winfiles\Settings\clink-gizmos"
+)
+
+:: Sparse checkout Linux dotfiles repository
+if not exist "%USERPROFILE%\.dotfiles" (
+    cd "%USERPROFILE%"
     git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/eggbean/.dotfiles.git
-    cd %USERPROFILE%\.dotfiles
+    cd "%USERPROFILE%\.dotfiles"
     git sparse-checkout set --no-cone /.gitattributes .git-crypt .githooks bin/scripts config
     git checkout
     icacls "%CD%" /setowner "%USERNAME%" /t
 )
 
-:: Add bin directory from Windows Defender's exclusion list, because some binaries are
-:: getting false positives. This is a bit risky, so scheduled scans will be done.
-powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath ""$env:HOME\winfiles\bin"""
+:: Create symlinks between %APPDATA% and Linux dotfiles
+call :CreateSymlink "%APPDATA%\gnupg" "%USERPROFILE%\.dotfiles\config\.gnupg"
+call :CreateSymlink "%APPDATA%\copyq" "%USERPROFILE%\.dotfiles\config\.config\copyq"
+call :CreateSymlink "%APPDATA%\XnViewMP" "%USERPROFILE%\.dotfiles\config\.config\XnViewMP"
+call :CreateSymlink "%APPDATA%\GitHub CLI" "%USERPROFILE%\.dotfiles\config\.config\gh"
+call :CreateSymlink "%APPDATA%\mpv" "%USERPROFILE%\.dotfiles\config\.config\mpv"
+call :CreateSymlink "%APPDATA%\qutebrowser\config" "%USERPROFILE%\.dotfiles\config\.config\qutebrowser"
 
-:: Setup Clink
-if not exist %LOCALAPPDATA%\clink (
-    mkdir %LOCALAPPDATA%\clink
-)
-if exist %LOCALAPPDATA%\clink\clink_start.cmd (
-    del %LOCALAPPDATA%\clink\clink_start.cmd
-)
-mklink %LOCALAPPDATA%\clink\clink_start.cmd %USERPROFILE%\winfiles\scripts\clink_start.cmd
-if exist %LOCALAPPDATA%\clink\clink_settings (
-    del %LOCALAPPDATA%\clink\clink_settings
-)
-mklink %LOCALAPPDATA%\clink\clink_settings %USERPROFILE%\winfiles\Settings\clink_settings
+:: Create symlink for vimfiles from Linux dotfiles repository
+call :CreateSymlink "%USERPROFILE%\vimfiles" "%USERPROFILE%\.dotfiles\config\.config\vim"
+attrib /l +h "%USERPROFILE%\vimfiles"
 
-if exist %LOCALAPPDATA%\clink\_inputrc (
-    del %LOCALAPPDATA%\clink\_inputrc
-)
-mklink %LOCALAPPDATA%\clink\_inputrc %USERPROFILE%\winfiles\Settings\_inputrc
-
-if defined CLINK_COMPLETIONS_DIR (
-    if not exist "%CLINK_COMPLETIONS_DIR%" (
-        git clone https://github.com/vladimir-kotikov/clink-completions.git %USERPROFILE%\winfiles\Settings\clink-completions
-        clink installscripts "%USERPROFILE%\winfiles\Settings\clink-completions"
-    )
-)
-
-if not exist %USERPROFILE%\winfiles\Settings\clink-gizmos (
-    git clone https://github.com/chrisant996/clink-gizmos.git %USERPROFILE%\winfiles\Settings\clink-gizmos
-    clink installscripts %USERPROFILE%\winfiles\Settings\clink-gizmos
-)
-
-:: Symlink gnupg configuration
-if exist %APPDATA%\gnupg (
-    if exist "%APPDATA%\gnupg\*" (
-        rmdir /s /q %APPDATA%\gnupg
-    )
-)
-if not exist %APPDATA%\gnupg (
-    mklink /d %APPDATA%\gnupg %USERPROFILE%\.dotfiles\config\.gnupg
-)
-
-:: Symlink copyq configuration
-if exist %APPDATA%\copyq (
-    if exist "%APPDATA%\copyq\*" (
-        rmdir /s /q %APPDATA%\copyq
-    )
-)
-if not exist %APPDATA%\copyq (
-    mklink /d %APPDATA%\copyq %USERPROFILE%\.dotfiles\config\.config\copyq
-)
-
-:: Symlink XnViewMP config
-if exist %APPDATA%\XnViewMP (
-    if exist "%APPDATA%\XnViewMP\*" (
-        rmdir /s /q %APPDATA%\XnViewMP
-    )
-)
-if not exist %APPDATA%\XnViewMP (
-    mklink /d %APPDATA%\XnViewMP %USERPROFILE%\.dotfiles\config\.config\XnViewMP
-)
-
-:: Symlink gh configuration
-if exist %APPDATA%\"GitHub CLI" (
-    if exist "%APPDATA%\GitHub CLI\*" (
-        rmdir /s /q %APPDATA%\"GitHub CLI"
-    )
-)
-if not exist %APPDATA%\"GitHub CLI" (
-    mklink /d %APPDATA%\"GitHub CLI" %USERPROFILE%\.dotfiles\config\.config\gh
-)
-
-:: Symlink mpv configuration
-if exist %APPDATA%\mpv (
-    if exist "%APPDATA%\mpv\*" (
-        rmdir /s /q %APPDATA%\mpv
-    )
-)
-if not exist %APPDATA%\mpv (
-    mklink /d %APPDATA%\mpv %USERPROFILE%\.dotfiles\config\.config\mpv
-)
-
-:: Symlink qutebrowser configuration
-if exist %APPDATA%\qutebrowser\config (
-    if exist "%APPDATA%\qutebrowser\config\*" (
-        rmdir /s /q %APPDATA%\qutebrowser\config
-    )
-)
-if not exist %APPDATA%\qutebrowser (
-    mkdir %APPDATA%\qutebrowser
-)
-mklink /d %APPDATA%\qutebrowser\config %USERPROFILE%\.dotfiles\config\.config\qutebrowser
-
-:: Symlink vim configuration
-if not exist %USERPROFILE%\vimfiles (
-    mklink /d %USERPROFILE%\vimfiles %USERPROFILE%\.dotfiles\config\.config\vim
-    attrib /l +h %USERPROFILE%\vimfiles
-)
-if not exist %USERPROFILE%\vimfiles.lnk (
+:: Create vimfiles shortcut
+if not exist "%USERPROFILE%\vimfiles.lnk" (
     nircmd shortcut "%USERPROFILE%\.dotfiles\config\.config\vim" "%USERPROFILE%" vimfiles "%USERPROFILE%\winfiles\icons\my_icons\vimfiles.ico"
     echo vimfiles shortcut created
 )
 
-:: Copy any existing config files to repository
-if exist %USERPROFILE%\.config (
-    robocopy %USERPROFILE%\.config\ %USERPROFILE%\winfiles\Settings\.config\ /move /e /it /im >NUL
-    rmdir %USERPROFILE%\.config >nul 2>&1
+:: Move and symlink .config
+if exist "%USERPROFILE%\.config" (
+    robocopy "%USERPROFILE%\.config\" "%USERPROFILE%\winfiles\Settings\.config\" /move /e /it /im >NUL
+    rmdir "%USERPROFILE%\.config" >nul 2>&1
 )
-mklink /d %USERPROFILE%\.config %USERPROFILE%\winfiles\Settings\.config
-attrib /l +h %USERPROFILE%\.config
+call :CreateSymlink "%USERPROFILE%\.config" "%USERPROFILE%\winfiles\Settings\.config"
 
-:: Symlink dotfiles
+:: Symlink other dotfiles
 for %%S in (.digrc .envrc .profile .ripgreprc) do (
-    if exist %USERPROFILE%\%%S (
-        del /a %USERPROFILE%\%%S
-    )
-    mklink %USERPROFILE%\%%S %USERPROFILE%\winfiles\Settings\%%S
-    attrib /l +h %USERPROFILE%\%%S
+    call :CreateSymlink "%USERPROFILE%\%%S" "%USERPROFILE%\winfiles\Settings\%%S"
 )
 
 :: Symlink WinSCP config
-if not exist %LOCALAPPDATA%\Programs\WinSCP (
-    mkdir%LOCALAPPDATA%\Programs\WinSCP
+if not exist "%LOCALAPPDATA%\Programs\WinSCP" (
+    mkdir "%LOCALAPPDATA%\Programs\WinSCP"
 )
-if exist %LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini (
-    rm %LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini
-    mklink %LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini %USERPROFILE%\winfiles\Settings\WinSCP.ini
+if exist "%LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini" (
+    del "%LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini"
 )
+mklink "%LOCALAPPDATA%\Programs\WinSCP\WinSCP.ini" "%USERPROFILE%\winfiles\Settings\WinSCP.ini"
 
 :: Copy SumatraPDF config
-if not exist %LOCALAPPDATA%\SumatraPDF (
-    mkdir %LOCALAPPDATA%\SumatraPDF
-    copy %USERPROFILE%\winfiles\Settings\SumatraPDF-settings.txt %LOCALAPPDATA%\SumatraPDF
+if not exist "%LOCALAPPDATA%\SumatraPDF" (
+    mkdir "%LOCALAPPDATA%\SumatraPDF"
+    copy "%USERPROFILE%\winfiles\Settings\SumatraPDF-settings.txt" "%LOCALAPPDATA%\SumatraPDF"
 )
 
 :: Symlink Windows Terminal config
-if not exist %LOCALAPPDATA%\Packages\Microsoft\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState (
-    mkdir %LOCALAPPDATA%\Packages\Microsoft\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+if not exist "%LOCALAPPDATA%\Packages\Microsoft\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" (
+    mkdir "%LOCALAPPDATA%\Packages\Microsoft\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 )
-if exist %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json (
-    del %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+if exist "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" (
+    del "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 )
-if %USERNAME% == webadmin (
-    mklink %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\winfiles\Windows_Terminal\settings-ubuntu.json
+if "%USERNAME%" == "webadmin" (
+    mklink "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "%USERPROFILE%\winfiles\Windows_Terminal\settings-ubuntu.json"
 ) else (
-    mklink %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\winfiles\Windows_Terminal\settings.json
-)
-
-:: Make startup shortcut for CopyQ
-if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\copyq.lnk" (
-    nircmd shortcut "C:\Program Files\CopyQ\copyq.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" CopyQ
-    echo CopyQ startup shortcut created
+    mklink "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "%USERPROFILE%\winfiles\Windows_Terminal\settings.json"
 )
 
 :: Determine computer chassis type
 for /f "delims={}" %%i in ('wmic systemenclosure get chassistypes ^| findstr "{"') do @set "chassistype=%%i"`
 
-:: Make startup shortcut for tpmiddle-rs on desktops
+:: Create startup shortcuts
+call :CreateStartupShortcut "CopyQ" "C:\Program Files\CopyQ\copyq.exe"
+call :CreateStartupShortcut "Sizer" "C:\Program Files (x86)\Sizer\sizer.exe"
+call :CreateStartupShortcut "SylphyHorn" "%USERPROFILE%\winfiles\SylphyHorn\SylphyHorn.exe"
+call :CreateStartupShortcut "Quake Terminal" "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe" "-w _quake -p ~qCommand Prompt~q" "%USERPROFILE%\winfiles\icons\app_icons\terminal.ico" "" "min"
+
+:: Create startup shortcut for tpmiddle-rs on ThinkStation desktops
 if %chassistype% GEQ 3 if %chassistype% LEQ 7 (
-    if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\tpmiddle-rs.lnk" (
-        nircmd shortcut "%USERPROFILE%\winfiles\bin\tpmiddle-rs.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" tpmiddle-rs
-        echo tpmiddle-rs startup shortcut created
-    )
+    call :CreateStartupShortcut "tpmiddle-rs" "%USERPROFILE%\winfiles\bin\tpmiddle-rs.vbs"
 )
 
-:: Make startup shortcut for MarbleScroll on laptops
+:: Create startup shortcut for MarbleScroll on ThinkPad laptops
 if %chassistype% GEQ 8 if %chassistype% LEQ 10 (
-    if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\MarbleScroll.lnk" (
-        nircmd shortcut "%USERPROFILE%\winfiles\bin\MarbleScroll.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" MarbleScroll
-        echo MarbleScroll startup shortcut created
-    )
+    call :CreateStartupShortcut "MarbleScroll" "%USERPROFILE%\winfiles\bin\MarbleScroll.exe"
 )
 
-:: Make startup shortcut for Sizer
-if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Sizer.lnk" (
-    nircmd shortcut "C:\Program Files (x86)\Sizer\sizer.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" Sizer
-    echo Sizer startup shortcut created
-)
-
-:: Make startup shortcut for SylphyHornPlus11
-if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SylphyHorn.lnk" (
-    nircmd shortcut "%USERPROFILE%\winfiles\SylphyHorn\SylphyHorn.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" SylphyHorn
-    echo SylphyHorn startup shortcut created
-)
-
-:: Make startup shortcut for Quake Terminal
-if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Quake Terminal.lnk" (
-    nircmd shortcut "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" "Quake Terminal" "-w _quake -p ~qCommand Prompt~q" "%USERPROFILE%\winfiles\icons\app_icons\terminal.ico" "" min
-    echo Quake Terminal startup shortcut created
-)
-
-:: Install fonts
-pushd %USERPROFILE%\winfiles\fonts
+:: Install and register fonts
+pushd "%USERPROFILE%\winfiles\fonts"
 for /d %%F in (*) do (
     pushd %%F
     for %%x in (*.ttf *.otf) do (
         set "fontInstalled=false"
         for /f "tokens=3*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" ^| findstr /i "%%~nx"') do set "fontInstalled=true"
         if "!fontInstalled!"=="false" (
-            %USERPROFILE%\winfiles\bin\fontreg /copy
+            "%USERPROFILE%\winfiles\bin\fontreg" /copy
             echo %%~nxF fonts installed
         )
     )
@@ -272,13 +179,10 @@ for /d %%F in (*) do (
 )
 popd
 
-:: Install the wedge redirector for the Chrometana Pro Chrome extension
-powershell -File "%~dp0install_wedge.ps1"
+:: Enable Developer Mode (allows symlink creation without elevation)
+powershell -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx' -Name 'AllowDevelopmentWithoutDevLicense' -Value 1"
 
-:: Set icons for various folders
-call "%~dp0fix_icons.cmd"
-
-:: Enable hibernation
+:: Enable Hibernation
 powercfg /hibernate on
 powershell -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings' -Name 'ShowHibernateOption' -Value 1"
 
@@ -304,10 +208,49 @@ powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Cu
 :: Disable Snap Layouts on top of screen
 powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'EnableSnapBar' -Value 0"
 
+:: Disable keyboard annoyances
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\StickyKeys' -Name 'Flags' -Value 506"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name 'Flags' -Value 122"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\ToggleKeys' -Name 'Flags' -Value 58"
+
+:: Enable Windows Features
+powershell -Command "Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' | Out-Null"
+powershell -Command "Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName 'VirtualMachinePlatform' | Out-Null"
+powershell -Command "Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName 'Containers-DisposableClientVM' | Out-Null"
+
+:: Disable Windows Features
+powershell -Command "Disable-WindowsOptionalFeature -NoRestart -Online -FeatureName 'WindowsMediaPlayer' | Out-Null"
+powershell -Command "Disable-WindowsOptionalFeature -NoRestart -Online -FeatureName 'Printing-XPSServices-Features' | Out-Null"
+
 :: Hide dotfiles and dotdirectories in %USERPROFILE% and winfiles
-for /f %%D in ('dir /b /a:-h %USERPROFILE%\.*') do attrib +h %USERPROFILE%\%%D
-for /f %%E in ('dir /b /a:-h %USERPROFILE%\winfiles\.*') do attrib +h %USERPROFILE%\winfiles\%%E
+for /f %%D in ('dir /b /a:-h "%USERPROFILE%\.*"') do attrib +h "%USERPROFILE%\%%D"
+for /f %%E in ('dir /b /a:-h "%USERPROFILE%\winfiles\.*"') do attrib +h "%USERPROFILE%\winfiles\%%E"
 
 :: Set clink to autorun for all users
 :: (this is done at the end as it seems to terminate the script)
 clink autorun -a install >nul 2>&1
+exit /b 0
+
+:: Function to create symlink after deleting existing directory
+:CreateSymlink
+if exist "%1" (
+    rmdir /s /q "%1"
+)
+mklink /d "%1" "%2"
+goto :eof
+
+:: Function to create a startup shortcut using nircmd
+:CreateStartupShortcut
+setlocal
+set "name=%~1"
+set "target=%~2"
+set "arguments=%~3"
+set "icon=%~4"
+set "start_in=%~5"
+set "window_style=%~6"
+if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%name%.lnk" (
+    nircmd shortcut "%target%" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" "%name%" "%icon%" "%start_in%" "%arguments%" "%window_style%"
+    echo %name% startup shortcut created
+)
+endlocal
+goto :eof
