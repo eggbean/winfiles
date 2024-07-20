42673e8405483c50a6000aed19721191b7bcf968
eggbean <815311+eggbean@users.noreply.github.com>
2024-07-20 10:11:52 +0100

    Update winget installed packages

--- scripts/install_packages.cmd
+++ scripts/install_packages.cmd
@@ -10,7 +10,6 @@ if not %ERRORLEVEL% == 0 (
 :: Install essential packages
 winget install -e --id Git.Git
 winget install -e --id Google.Chrome
-winget install -e --id M2Team.NanaZip
 winget install -e --id Microsoft.VCRedist.2015+.x64
 winget install -e --id Microsoft.VCRedist.2015+.x86
 winget install -e --id Microsoft.WindowsTerminal
@@ -19,6 +18,7 @@ winget install -e --id Mythicsoft.AgentRansack
 winget install -e --id Notion.Notion
 winget install -e --id SomePythonThings.WingetUIStore --override "/SP /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NoAutoStart /ALLUSERS /LANG=english"
 winget install -e --id WinDirStat.WinDirStat --include-unknown
+winget install -e --id XnSoft.XnViewMP
 winget install -e --id chrisant996.Clink
 winget install -e --id gerardog.gsudo
 winget install -e --id hluk.CopyQ
@@ -47,6 +47,7 @@ if "%USERNAME%" == "jason" (
     winget install -e --id Discord.Discord
     winget install -e --id GnuPG.Gpg4win
     winget install -e --id Google.GoogleDrive
+    winget install -e --id M2Team.NanaZip
     winget install -e --id Microsoft.PowerToys --accept-source-agreements -h --disable-interactivity --accept-package-agreements
     winget install -e --id OpenWhisperSystems.Signal
     winget install -e --id Oracle.VirtualBox --skip-dependencies
@@ -57,7 +58,6 @@ if "%USERNAME%" == "jason" (
     winget install -e --id SlackTechnologies.Slack
     winget install -e --id Soulseek.SoulseekQt
     winget install -e --id SumatraPDF.SumatraPDF
-    winget install -e --id VideoLAN.VLC
     winget install -e --id VirusTotal.VirusTotalUploader
     winget install -e --id WinMerge.WinMerge
     winget install -e --id WinSCP.WinSCP
