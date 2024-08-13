e8d62cf1c40e901b66a056f78446b51e18eb3e3f
eggbean <815311+eggbean@users.noreply.github.com>
2024-08-13 19:50:08 +0100

    Add PicPick to packages

--- scripts/install_packages.cmd
+++ scripts/install_packages.cmd
@@ -16,6 +16,7 @@ winget install -e --id Microsoft.VCRedist.2015+.x86
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Mozilla.Firefox
 winget install -e --id Mythicsoft.AgentRansack
+winget install -e --id NGWIN.PicPick
 winget install -e --id Notion.Notion --no-upgrade
 winget install -e --id SomePythonThings.WingetUIStore --override "/SP /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NoAutoStart /ALLUSERS /LANG=english"
 winget install -e --id WinDirStat.WinDirStat --include-unknown
