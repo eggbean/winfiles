d38a401dadbea9c32e1f79102dd7b1bd79873615
eggbean <815311+eggbean@users.noreply.github.com>
2024-09-08 21:59:48 +0100

    Add more sfsu commands to scoop-wrapper.cmd

--- scripts/scoop-wrapper.cmd
+++ scripts/scoop-wrapper.cmd
@@ -1,8 +1,23 @@
 @echo off
 
 if "%1" == "search" goto search_subroutine
-if "%1" == "list" goto sfsu_subroutine
+if "%1" == "app" goto sfsu_subroutine
+if "%1" == "bucket" goto sfsu_subroutine
+if "%1" == "cache" goto sfsu_subroutine
+if "%1" == "cat" goto sfsu_subroutine
+if "%1" == "checkup" goto sfsu_subroutine
+if "%1" == "depends" goto sfsu_subroutine
+if "%1" == "describe" goto sfsu_subroutine
+if "%1" == "download" goto sfsu_subroutine
+if "%1" == "export" goto sfsu_subroutine
+if "%1" == "home" goto sfsu_subroutine
 if "%1" == "info" goto sfsu_subroutine
+if "%1" == "list" goto sfsu_subroutine
+if "%1" == "outdated" goto sfsu_subroutine
+if "%1" == "scan" goto sfsu_subroutine
+if "%1" == "status" goto sfsu_subroutine
+if "%1" == "unused-buckets" goto sfsu_subroutine
+if "%1" == "update" goto sfsu_subroutine
 powershell scoop.ps1 %*
 goto :EOF
 
