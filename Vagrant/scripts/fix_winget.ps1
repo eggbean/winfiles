a7928abe003ae4b1f8ca33438dc2b5f52282f78f
eggbean <815311+eggbean@users.noreply.github.com>
2024-10-18 14:54:27 +0100

    Add Vagrantfile

---
+++ Vagrant/scripts/fix_winget.ps1
@@ -0,0 +1,23 @@
+# Fix winget --source winget on Vagrant boxes
+# [ELEVATED]
+try {
+    # Download UI.Xaml
+    $xamlPath = ".\Microsoft.UI.Xaml.2.8.x64.appx"
+    Start-BitsTransfer -Source "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx" -Destination $xamlPath
+    Write-Host "Downloaded Microsoft.UI.Xaml"
+
+    # Install UI.Xaml
+    Add-AppxPackage -Path $xamlPath
+    Write-Host "Installed Microsoft.UI.Xaml"
+
+    # Download Winget Installer
+    $wingetPath = ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/download/v1.9.2411-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination $wingetPath
+    Write-Host "Downloaded Winget Installer"
+
+    # Install Winget
+    Add-AppxPackage -Path $wingetPath
+    Write-Host "Installed Winget"
+} catch {
+    Write-Error "Error during Winget setup: $_"
+}
