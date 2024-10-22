4170875160aacafdc082bf249e2b16ba688c9c52
eggbean <815311+eggbean@users.noreply.github.com>
2024-10-22 20:40:59 +0100

    Add winget --source to package list
    This will avoid some errors when winget fails to find packages in msstore
    which it seems to do sometimes. It probably speeds the script up as well
    as the msstore doesn't need to be queried for every package.

--- scripts/install_packages.ps1
+++ scripts/install_packages.ps1
@@ -1,83 +1,81 @@
 # Install packages using winget
 
-# Check if the script is running with admin privileges
+# Ensure the script is run with administrative privileges
 $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
 $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
 if (-Not $isAdmin) {
-    Write-Output "Not admin/elevated"
+    Write-Error "This script needs to be run as Administrator."
     exit 1
 }
 
 # Install essential packages
-winget install -e --id Git.Git --override "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=git_options.ini"
-winget install -e --id Google.Chrome
-winget install -e --id Microsoft.PowerShell
-winget install -e --id Microsoft.VCRedist.2015+.x64
-winget install -e --id Microsoft.VCRedist.2015+.x86
-winget install -e --id Microsoft.WindowsTerminal
-winget install -e --id Mozilla.Firefox
-winget install -e --id Mythicsoft.AgentRansack
-winget install -e --id Notion.Notion --no-upgrade
-winget install -e --id Python.Python.3.13
-winget install -e --id SomePythonThings.WingetUIStore --override "/SP /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NoAutoStart /ALLUSERS /LANG=english"
-winget install -e --id WinDirStat.WinDirStat
-winget install -e --id chrisant996.Clink
-winget install -e --id gerardog.gsudo
-winget install -e --id hluk.CopyQ
-winget install -e --id qutebrowser.qutebrowser -l "C:\Program Files\qutebrowser"
-winget install -e --id vim.vim
-winget install -e --id wez.wezterm
-
+winget install --source winget -e --id Git.Git --override "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=git_options.ini"
+winget install --source winget -e --id Google.Chrome
+winget install --source winget -e --id Microsoft.PowerShell
+winget install --source winget -e --id Microsoft.VCRedist.2015+.x64
+winget install --source winget -e --id Microsoft.VCRedist.2015+.x86
+winget install --source winget -e --id Microsoft.WindowsTerminal
+winget install --source winget -e --id Mozilla.Firefox
+winget install --source winget -e --id Mythicsoft.AgentRansack
+winget install --source winget -e --id Notion.Notion --no-upgrade
+winget install --source winget -e --id Python.Python.3.13
+winget install --source winget -e --id SomePythonThings.WingetUIStore --override "/SP /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NoAutoStart /ALLUSERS /LANG=english"
+winget install --source winget -e --id WinDirStat.WinDirStat
+winget install --source winget -e --id chrisant996.Clink
+winget install --source winget -e --id gerardog.gsudo
+winget install --source winget -e --id hluk.CopyQ
+winget install --source winget -e --id qutebrowser.qutebrowser -l "C:\Program Files\qutebrowser"
+winget install --source winget -e --id vim.vim
+winget install --source winget -e --id wez.wezterm
 
 # Install software for desktop computers
 # Check if the chassis type indicates a desktop computer (values between 3 and 7)
 $chassisType = (Get-WmiObject -Class Win32_SystemEnclosure).ChassisTypes
 if ($chassisType -ge 3 -and $chassisType -le 7 -and $env:USERNAME -ne "vagrant") {
-    winget install -e --id CreativeTechnology.CreativeApp
-    winget install -e --id Dell.DisplayManager -v 1.56.2110
-    winget install -e --id Logitech.OptionsPlus --skip-dependencies
-    winget install -e --id xanderfrangos.twinkletray
+    winget install --source winget -e --id CreativeTechnology.CreativeApp
+    winget install --source winget -e --id Dell.DisplayManager -v 1.56.2110
+    winget install --source winget -e --id Logitech.OptionsPlus --skip-dependencies
+    winget install --source winget -e --id xanderfrangos.twinkletray
 }
 
 # Install packages for personal account
 if ($env:USERNAME -eq "jason" -or $env:USERNAME -eq "vagrant") {
-    winget install -e --id 9NKSQGP7F2NH --accept-package-agreements   # WhatsApp
-    winget install -e --id 9P3JFR0CLLL6 --accept-package-agreements   # mpv (Unofficial)
-    winget install -e --id Amazon.SendToKindle --skip-dependencies
-    winget install -e --id Audacity.Audacity
-    winget install -e --id BrianApps.Sizer
-    winget install -e --id CPUID.CPU-Z
-    winget install -e --id CrystalDewWorld.CrystalDiskInfo
-    winget install -e --id CrystalDewWorld.CrystalDiskMark
-    winget install -e --id Discord.Discord --no-upgrade
-    winget install -e --id GnuPG.Gpg4win
-    winget install -e --id GoLang.Go
-    winget install -e --id Google.GoogleDrive
-    winget install -e --id Hashicorp.Vagrant
-    winget install -e --id LocalSend.LocalSend
-    winget install -e --id M2Team.NanaZip
-    winget install -e --id Microsoft.PowerToys --accept-source-agreements -h --disable-interactivity --accept-package-agreements
-    winget install -e --id NGWIN.PicPick
-    winget install -e --id NathanBeals.WinSSH-Pageant
-    winget install -e --id Nikkho.FileOptimizer
-    winget install -e --id OpenWhisperSystems.Signal
-    winget install -e --id Pushbullet.Pushbullet
-    winget install -e --id REALiX.HWiNFO
-    winget install -e --id RustemMussabekov.Raindrop
-    winget install -e --id Samsung.DeX
-    winget install -e --id SergeySerkov.TagScanner
-    winget install -e --id SlackTechnologies.Slack
-    winget install -e --id Soulseek.SoulseekQt
-    winget install -e --id SumatraPDF.SumatraPDF
-    winget install -e --id VirusTotal.VirusTotalUploader
-    winget install -e --id WinMerge.WinMerge
-    winget install -e --id WinSCP.WinSCP
-    winget install -e --id WiresharkFoundation.Wireshark
-    winget install -e --id Xanashi.Icaros --source winget
-    winget install -e --id XnSoft.XnViewMP
-    winget install -e --id ente-io.auth-desktop --skip-dependencies
-    winget install -e --id flyingpie.windows-terminal-quake   # actually for wezterm
-    winget install -e --id qBittorrent.qBittorrent
+    winget install --source msstore -e --id 9NKSQGP7F2NH --accept-package-agreements   # WhatsApp
+    winget install --source msstore -e --id 9P3JFR0CLLL6 --accept-package-agreements   # mpv (Unofficial)
+    winget install --source winget -e --id Amazon.SendToKindle --skip-dependencies
+    winget install --source winget -e --id Audacity.Audacity
+    winget install --source winget -e --id BrianApps.Sizer
+    winget install --source winget -e --id CPUID.CPU-Z
+    winget install --source winget -e --id CrystalDewWorld.CrystalDiskInfo
+    winget install --source winget -e --id CrystalDewWorld.CrystalDiskMark
+    winget install --source winget -e --id Discord.Discord --no-upgrade
+    winget install --source winget -e --id GnuPG.Gpg4win
+    winget install --source winget -e --id GoLang.Go
+    winget install --source winget -e --id Google.GoogleDrive
+    winget install --source winget -e --id Hashicorp.Vagrant
+    winget install --source winget -e --id LocalSend.LocalSend
+    winget install --source winget -e --id M2Team.NanaZip
+    winget install --source winget -e --id Microsoft.PowerToys --accept-source-agreements -h --disable-interactivity --accept-package-agreements
+    winget install --source winget -e --id NGWIN.PicPick
+    winget install --source winget -e --id NathanBeals.WinSSH-Pageant
+    winget install --source winget -e --id Nikkho.FileOptimizer
+    winget install --source winget -e --id OpenWhisperSystems.Signal
+    winget install --source winget -e --id Pushbullet.Pushbullet
+    winget install --source winget -e --id REALiX.HWiNFO
+    winget install --source winget -e --id RustemMussabekov.Raindrop
+    winget install --source winget -e --id Samsung.DeX
+    winget install --source winget -e --id SergeySerkov.TagScanner
+    winget install --source winget -e --id SlackTechnologies.Slack
+    winget install --source winget -e --id Soulseek.SoulseekQt
+    winget install --source winget -e --id SumatraPDF.SumatraPDF
+    winget install --source winget -e --id VirusTotal.VirusTotalUploader
+    winget install --source winget -e --id WinMerge.WinMerge
+    winget install --source winget -e --id WinSCP.WinSCP
+    winget install --source winget -e --id WiresharkFoundation.Wireshark
+    winget install --source winget -e --id Xanashi.Icaros
+    winget install --source winget -e --id XnSoft.XnViewMP
+    winget install --source winget -e --id ente-io.auth-desktop --skip-dependencies
+    winget install --source winget -e --id flyingpie.windows-terminal-quake   # actually for wezterm
+    winget install --source winget -e --id qBittorrent.qBittorrent
 }
