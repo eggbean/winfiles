4ca0b1550af71e6b898871858dd8db6e0bec19e3
eggbean <815311+eggbean@users.noreply.github.com>
2024-10-25 18:16:44 +0100

    Update Set-Symlink function to use Write-OutputWithIcon

--- scripts/Set-Symlink.psm1
+++ scripts/Set-Symlink.psm1
@@ -13,27 +13,27 @@ function Set-Symlink {
 
             # If the symlink already points to the correct target, no need to recreate it
             if ($currentTarget -eq $Target) {
-                echo ":white_check_mark: Symlink already exists and points to the correct target." | gum format -t emoji
+                Write-OutputWithIcon -Message "Symlink already exists and points to the correct target." -IconType "info"
                 return
             } else {
-                echo ":warning: Symlink exists but points to a different target. Removing it..." | gum format -t emoji
+                Write-OutputWithIcon -Message "Symlink exists but points to a different target. Removing it..." -IconType "warning"
                 Remove-Item -Force $Link
             }
         } else {
             # It's not a symlink, delete it
-            echo ":warning: Removing existing '$Link' (not a symlink)..." | gum format -t emoji
+            Write-OutputWithIcon -Message "Removing existing '$Link' (not a symlink)..." -IconType "warning"
             Remove-Item -Recurse -Force $Link
         }
     }
 
     # Create the symlink
-    echo ":construction: Creating symlink from '$Link' to '$Target'" | gum format -t emoji
+    Write-OutputWithIcon -Message "Creating symlink from '$Link' to '$Target'" -IconType "working"
     New-Item -ItemType SymbolicLink -Path $Link -Target $Target -Force -ErrorAction Stop > $null
 
     # Check if creation was successful
     if ($?) {
-        echo ":white_check_mark: Symlink for '$Link' created successfully." | gum format -t emoji
+        Write-OutputWithIcon -Message "Symlink for '$Link' created successfully." -IconType "success"
     } else {
-        echo ":x: Failed to create symlink for '$Link'." | gum format -t emoji
+        Write-OutputWithIcon -Message "Failed to create symlink for '$Link'." -IconType "error"
     }
 }
